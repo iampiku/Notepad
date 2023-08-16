97c1f0f7801bb069e3841ea5a1883a4f2b8c3285
Pradipta Chatterjee <pradipta.chatterjee@globalids.com>
2023-08-16 21:01:29 +0530

    📝 notes card modifications

--- src/components/Notes/NotesGrid.vue
+++ src/components/Notes/NotesGrid.vue
@@ -1,14 +1,19 @@
 <template>
-	<div
-		class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-2/3"
-	>
-		<NoteCard
-			:key="index"
-			:note="note"
-			@onEdit="handleEdit"
-			@onRemove="handleRemove"
-			v-for="(note, index) in noteStore.notes"
-		></NoteCard>
+	<div class="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-2/3">
+		<template v-if="noteStore.noteCount !== 0">
+			<NoteCard
+				:key="index"
+				:note="note"
+				@onEdit="handleEdit"
+				@onRemove="handleRemove"
+				v-for="(note, index) in noteStore.notes"
+			></NoteCard>
+		</template>
+		<template v-else>
+			<span class="text-2xl font-mono text-red-400 place-items-center"
+				>No notes added.</span
+			>
+		</template>
 	</div>
 </template>
 
