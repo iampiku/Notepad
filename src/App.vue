b0c22b7e18e472cb1188d368df2621a0d4ea2668
iampiku <iampikuchatterjee@gmail.com>
2023-08-16 11:35:10 +0530

    🍍 added pinia store for state management

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-	<main>
+	<main class="flex justify-center pt-20">
 		<ThemeSwitch
 			class="absolute top-7 right-6"
 			@onThemeChange="handleThemeChange"
 		></ThemeSwitch>
 		<AddNoteModal @onAddNote="handleAddNote"></AddNoteModal>
+		<EditNoteModal @onEditNote="handleEditNote"></EditNoteModal>
+		<NotesGrid></NotesGrid>
 	</main>
 </template>
 
@@ -14,13 +16,24 @@
  */
 import AddNoteModal from "@/components/Modals/AddNoteModal.vue";
 import ThemeSwitch from "@/components/ThemeSwitch.vue";
+import NotesGrid from "@/components/Notes/NotesGrid.vue";
 
 /**
  * Interface
  */
 import INote from "@/interface/INote";
 
+/**
+ * Store
+ */
+import { useNoteStore } from "@/store/NoteStore";
+const noteStore = useNoteStore();
+
 function handleAddNote(note: INote) {
+	noteStore.createNewNote(note);
+}
+
+function handleEditNote(note: INote) {
 	console.log(note);
 }
 
