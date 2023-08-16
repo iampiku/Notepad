e64d325beb0e3a9e4d0c5cae2b9e696f6379802a
iampiku <iampikuchatterjee@gmail.com>
2023-08-16 11:38:21 +0530

    🗑️ implemented delete modal for delete action

---
+++ src/components/Modals/DeleteNoteModal.vue
@@ -0,0 +1,23 @@
+<template>
+	<Modal v-model="showModal">
+		<template #body>
+			<div class="card w-96 bg-base-100 shadow-xl text-neutral-content">
+				<div class="card-body items-center text-center">
+					<h2 class="card-title">Delete Note!</h2>
+					<p>{{ props.message }}</p>
+				</div>
+			</div>
+		</template>
+	</Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from "@/components/Modals/Modal.vue";
+
+interface Props {
+	message: string;
+	showModal: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
