cdb93bdbc6c4d8a595a75e74e169c76031b80c4b
Pradipta Chatterjee <pradipta.chatterjee@globalids.com>
2023-08-18 20:43:40 +0530

    🔨 fixed app height

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-	<main class="flex justify-center pt-20">
+	<main class="flex justify-center pt-20 min-h-screen">
 		<ThemeSwitch
 			class="absolute top-7 right-6"
 			@onThemeChange="handleThemeChange"
 		></ThemeSwitch>
 		<AddNoteModal @onAddNote="handleAddNote"></AddNoteModal>
-		<!-- <EditNoteModal @onEditNote="handleEditNote"></EditNoteModal> -->
-		<NotesGrid></NotesGrid>
+		<EditNoteModal
+			:showModal="showEditModal"
+			@onEditNote="handleEditNote"
+		></EditNoteModal>
+		<!-- <DeleteNoteModal :message=""></DeleteNoteModal> -->
+		<NotesGrid
+			@onEdit="handleEditAction"
+			@onRemove="handleRemoveAction"
+		></NotesGrid>
 		<NotificationToast
 			type="success"
 			v-if="showNotification"
@@ -25,7 +32,8 @@ import { ref } from "vue";
  * Components
  */
 import AddNoteModal from "@/components/Modals/AddNoteModal.vue";
-// import EditNoteModal from "@/components/Modals/EditNoteModal.vue";
+import EditNoteModal from "@/components/Modals/EditNoteModal.vue";
+import DeleteNoteModal from "@/components/Modals/DeleteNoteModal.vue";
 import ThemeSwitch from "@/components/ThemeSwitch.vue";
 import NotesGrid from "@/components/Notes/NotesGrid.vue";
 import NotificationToast from "@/components/NotificationToast.vue";
@@ -46,9 +54,9 @@ function handleAddNote(note: INote) {
 	showToast();
 }
 
-// function handleEditNote(note: INote) {
-// 	console.log(note);
-// }
+function handleEditNote(note: INote) {
+	console.log(note);
+}
 
 function handleThemeChange(isDark: boolean) {
 	document.documentElement.setAttribute(
@@ -63,5 +71,17 @@ function showToast() {
 	setTimeout(() => {
 		showNotification.value = false;
 	}, 3000);
+}
+
+const showEditModal = ref<boolean>(false);
+const editNote = ref<INote>();
+function handleEditAction(note: INote) {
+	editNote.value = note;
+	showEditModal.value = true;
+}
+
+function handleRemoveAction(note: INote) {
+	const index: number = noteStore.notes.indexOf(note);
+	noteStore.removeNote(index);
 }
 </script>
