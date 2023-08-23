6784adabde002c2be2c22cbf20f008e324d6f874
Pradipta Chatterjee <pradipta.chatterjee@globalids.com>
2023-08-23 20:39:50 +0530

    🛠️ new design change implemented with drag and drop functionality 🔥

--- src/components/Notes/NoteCard.vue
+++ src/components/Notes/NoteCard.vue
@@ -1,79 +1,81 @@
 <template>
 	<div
-		class="card card-compact bg-primary-content shadow-xl border-gray-200 border-2"
+		draggable="true"
+		class="card card-bordered bg-white p-4 text-sm cursor-move max-w-xs"
 	>
-		<div class="card-body p-4">
-			<h2 class="card-title">{{ note.title }}</h2>
-			<p>{{ note.note }}</p>
-
-			<div
-				class="badge badge-secondary pb-3 font-bold tooltip"
-				data-tip="Status"
+		<div class="card-title font-bold pb-2 justify-between">
+			<span>{{ props.note.title }}</span>
+			<button
+				type="button"
+				class="btn btn-xs btn-ghost btn-circle"
+				@click="showMenu = !showMenu"
 			>
-				{{ note.status }}
-			</div>
-
-			<div class="card-actions justify-between align-center gap-2 flex-nowrap">
-				<span class="pt-1">{{ note.createdAt }}</span>
-				<div class="join">
-					<div class="tooltip" data-tip="Edit">
-						<button
-							class="btn btn-sm btn-primary join-item"
-							@click="emit('onEdit', note)"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-								/>
-							</svg>
-						</button>
-					</div>
-					<div class="tooltip" data-tip="Delete">
-						<button
-							class="btn btn-sm btn-primary join-item"
-							@click="emit('onRemove', note)"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								stroke="currentColor"
-								class="w-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-								/>
-							</svg>
-						</button>
-					</div>
-				</div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+					/>
+				</svg>
+			</button>
+			<Menu
+				v-if="showMenu"
+				class="absolute right-1 top-11"
+				@on-edit="handleAction('edit')"
+				@on-delete="handleAction('delete')"
+			></Menu>
+		</div>
+		<p class="pb-4">{{ props.note.note }}</p>
+		<div class="card-action">
+			<div class="text-xs font-bold flex items-center gap-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+					/>
+				</svg>
+				<span>{{ props.note.createdAt }}</span>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
 import INote from "@/interface/INote";
+
+import Menu from "@/components/Menus.vue";
 
 interface Props {
 	note: INote;
 }
-defineProps<Props>();
-
+const props = defineProps<Props>();
 const emit = defineEmits<{
 	(e: "onEdit", note: INote): void;
-	(e: "onRemove", note: INote): void;
+	(e: "onDelete", note: INote): void;
 }>();
+
+const showMenu = ref<boolean>(false);
+
+function handleAction(actionType: "edit" | "delete") {
+	if (actionType === "edit") emit("onEdit", props.note);
+	else emit("onDelete", props.note);
+	showMenu.value = false;
+}
 </script>
--- src/components/Notes/NotesGrid.vue
+++ src/components/Notes/NotesGrid.vue
@@ -1,38 +1,80 @@
 <template>
 	<div
-		class="grid gap-3 grid-rows-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-2/3"
+		id="todo-container"
+		class="grid gap-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 bg-slate-50 p-6 my-20 rounded-lg"
 	>
-		<template v-if="noteStore.noteCount !== 0">
-			<NoteCard
-				:key="index"
-				:note="note"
-				@onEdit="handleEdit"
-				@onRemove="handleRemove"
-				v-for="(note, index) in noteStore.notes"
-			></NoteCard>
-		</template>
-		<template v-else>
-			<EmptyNotes class="col-span-full row-span-full"></EmptyNotes>
-		</template>
+		<div
+			:key="index"
+			v-for="(note, index) in notes"
+			:class="`card card-bordered  ${note.bgColor}`"
+		>
+			<div class="card-title flex justify-between px-7 pt-6">
+				<span class="font-semibold text-base"
+					>{{ note.title }}
+					<span class="badge badge-sm rounded-xl badge-accent">
+						{{ note.notes.length }}</span
+					></span
+				>
+				<div class="tooltip" data-tip="Add Note">
+					<button
+						type="button"
+						class="btn btn-circle btn-xs btn-ghost"
+						@click="emit('onAdd')"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M12 4.5v15m7.5-7.5h-15"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+			<div
+				class="card-body p-4"
+				@dragover.prevent
+				@dragenter.prevent
+				@drop="onDrop($event, note.title)"
+			>
+				<div
+					:key="filteredNote.id"
+					v-for="filteredNote in note.notes"
+					@dragstart="handleDrag($event, filteredNote)"
+				>
+					<NoteCard
+						:note="filteredNote"
+						@on-edit="handleEdit"
+						@on-delete="handleDelete"
+					></NoteCard>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 /**
- * Component
+ * Vue
  */
-import NoteCard from "@/components/Notes/NoteCard.vue";
-import EmptyNotes from "@/components/Notes/EmptyNotes.vue";
+import { computed } from "vue";
 
 /**
  * Interface
  */
 import INote from "@/interface/INote";
 
-const emit = defineEmits<{
-	(e: "onEdit", note: INote): void;
-	(e: "onRemove", note: INote): void;
-}>();
+/**
+ * Component
+ */
+import NoteCard from "@/components/Notes/NoteCard.vue";
 
 /**
  * Store
@@ -40,11 +82,74 @@ const emit = defineEmits<{
 import { useNoteStore } from "@/store/NoteStore";
 const noteStore = useNoteStore();
 
-function handleEdit(note: INote): void {
-	emit("onEdit", note);
+const emit = defineEmits<{
+	(e: "onAdd"): void;
+	(e: "onEdit", note: INote): void;
+	(e: "onDelete", note: INote): void;
+}>();
+
+const notes = computed(() => {
+	return [
+		{
+			title: "Todo",
+			bgColor: "bg-gray-100",
+			notes: noteStore.todoNotes,
+		},
+		{
+			title: "In Progress",
+			bgColor: "bg-blue-100",
+			notes: noteStore.inProgressNotes,
+		},
+		{
+			title: "Pending",
+			bgColor: "bg-purple-100",
+			notes: noteStore.pendingNotes,
+		},
+		{
+			title: "Completed",
+			bgColor: "bg-green-100",
+			notes: noteStore.completedNotes,
+		},
+	];
+});
+
+function handleDrag(event: DragEvent, note: INote) {
+	if (event.dataTransfer) {
+		event.dataTransfer.dropEffect = "move";
+		event.dataTransfer.effectAllowed = "move";
+		event.dataTransfer.setData("noteId", note.id.toString());
+	}
 }
 
-function handleRemove(note: INote): void {
-	emit("onRemove", note);
+function onDrop(event: DragEvent, updatedStatus: string) {
+	if (event.dataTransfer) {
+		const noteId = event.dataTransfer.getData("noteId");
+		const note = noteStore.notes.find((note) => note.id === parseInt(noteId));
+		if (note) {
+			switch (updatedStatus) {
+				case "Todo":
+					note.status = "Todo";
+					break;
+				case "In Progress":
+					note.status = "Inprogress";
+					break;
+				case "Pending":
+					note.status = "Pending";
+					break;
+				case "Completed":
+					note.status = "Completed";
+					break;
+			}
+			noteStore.updateNote(note);
+		}
+	}
+}
+
+function handleEdit(note: INote) {
+	console.log(note);
+}
+
+function handleDelete(note: INote) {
+	noteStore.removeNote(note.id);
 }
 </script>
